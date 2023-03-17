dbcc0588e166f1501d9256f7cb4aae1a4a4aafd6
Mostafa Ashraf <64414166+mostafaashraf204@users.noreply.github.com>
2023-03-17 14:18:14 +0200

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,35 @@ chanespann(){
         this.myclass="colorr" 
       }
 }
+allStudents = [
+  {
+  name : "Mahmoud" ,
+  address : "Cairo Egypt",
+  date : "12/1/1995",
+  phone : "01234567890",
+  photo :"https://png.pngtree.com/element_our/20200610/ourmid/pngtree-default-avatar-image_2237213.jpg"
+  },
+  {
+  name : "ahmed" ,
+  address : "alex Egypt",
+  date : "8/1/1990",
+  phone : "01234567890",
+  photo :"https://png.pngtree.com/element_our/20200610/ourmid/pngtree-default-avatar-image_2237213.jpg"
+  },
+  {
+  name : "ali" ,
+  address : "Cairo Egypt",
+  date : "2/2/2000",
+  phone : "0123098765430",
+  photo :"https://png.pngtree.com/element_our/20200610/ourmid/pngtree-default-avatar-image_2237213.jpg"
+  },
+  {
+  name : "Osam" ,
+  address : "monefia Egypt",
+  date : "20/6/2006",
+  phone : "01234567890",
+  photo :"https://png.pngtree.com/element_our/20200610/ourmid/pngtree-default-avatar-image_2237213.jpg"
+  }
+
+];
 }
