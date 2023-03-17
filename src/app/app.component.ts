a15f81e5390b1070461726fc5e6989b5baa14f60
Mostafa Ashraf <64414166+mostafaashraf204@users.noreply.github.com>
2023-03-17 13:53:39 +0200

    count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'final-project';
+  num=0
+  myclass=""
+chanespan(){
+  this.num++
+  if(this.num>=10){
+this.myclass="color"
+  }
+  else if(this.num<=10){
+    this.myclass="colorr" 
+  }
+}
+chanespann(){
+  this.num--
+  if(this.num>=10){
+    this.myclass="color"
+      }
+      else if(this.num<=10){
+        this.myclass="colorr" 
+      }
+}
 }
